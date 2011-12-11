08ccf5f708901804e5931eb48ebc524c96468f54
Juanje Ojeda <jojeda@emergya.es>
2011-12-11 23:46:55 +0100

    Simplified the plugin home_users and rename the databag

--- home_users.rb
+++ home_users.rb
@@ -1,23 +1,16 @@
-provides 'home_users'
+provides 'users'
+
 require 'etc'
 
-home_users Mash.new
-
-etcpasswd = []
-
-Etc.passwd do |entry|
-  etcpasswd << entry
-end
+users Mash.new
 
 Dir["/home/*"].each do |homedir|
-  etcpasswd.each do |entry|
-    if homedir == entry.dir
-      home_users[homedir] = Mash.new(
-                                      :username => entry.name,
-                                      :gid      => entry.gid,
-                                      :uid      => entry.uid,
-                                      :gecos    => entry.gecos
-                                     )
-    end
+  Etc.passwd do |entry|
+    next unless homedir == entry.dir
+    users[entry.name] = Mash.new(
+      :username => entry.name,
+      :gid      => entry.gid,
+      :uid      => entry.uid
+    )
   end
 end
