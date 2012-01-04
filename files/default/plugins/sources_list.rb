aab366bf913d7f743b7299bc94449e3466fa35bb
Juanje Ojeda <jojeda@emergya.es>
2012-01-04 01:08:58 +0100

    Refactor: simplified in a more Ruby style
    Thanks to Ayose Cazorla for his comments about the code.
    There were also a bug. I was taken all the files at /etc/apt/sources.list.d/
    , but only the *.list are valid, the rest are invalid ones.

--- files/default/plugins/sources_list.rb
+++ files/default/plugins/sources_list.rb
@@ -2,45 +2,13 @@ provides 'sources_list'
 
 sources_list Mash.new
 
-def parse_sources(filename)
-
-  repos = []
-
-  f = File.open(filename, "r")
-  f.each_line do |repo|
-    if /^deb\s/.match(repo)
-      repos << repo
-    end
+Dir['/etc/apt/sources.list', '/etc/apt/sources.list.d/*.list'].each do |filename|
+  open(filename).readlines.grep(/^deb/).each do |line|
+    matches = line.match(/((?:http|https):\/\/[\S]*) (\w+|\w+-\w+) ([\w ]+)/)
+    url = matches[1].chomp('/')
+    sources_list[url] = Mash.new({
+                                    :suite      => matches[2],
+                                    :components => matches[3].split
+                                  })
   end
-  f.close()
-
-  return repos
-end
-
-
-repos = parse_sources('/etc/apt/sources.list')
-
-Dir["/etc/apt/sources.list.d/*"].each do |filename|
-  repos += parse_sources(filename)
-end
-
-repos.each do |repo|
-
-  tokens = repo.split()
-  repo_url = tokens[1]
-  branch = tokens[2]
-
-  if not sources_list.key?(repo_url)
-    sources_list[repo_url] = Mash.new()
-  end
-
-  if not sources_list[repo_url].key?(branch)
-    sources_list[repo_url][branch] = []
-  end
-
-  for i in 3..tokens.length-1
-    component = tokens[i]
-    sources_list[repo_url][branch] << component
-  end
-
 end
