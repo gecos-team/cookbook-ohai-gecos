09d202c662f172023c62a9039e8f836e90af444e
Roberto C. Morano <rcmorano@emergya.com>
2012-01-19 16:44:54 +0100

    fixed: now index is each server (url) and suite. before each index was only the server, resulting in overwriting each 'source_list' with the last suite

--- files/default/plugins/sources_list.rb
+++ files/default/plugins/sources_list.rb
@@ -6,9 +6,13 @@ Dir['/etc/apt/sources.list', '/etc/apt/sources.list.d/*.list'].each do |filename
   open(filename).readlines.grep(/^deb/).each do |line|
     matches = line.match(/((?:http|https):\/\/[\S]*) (\w+|\w+-\w+) ([\w ]+)/)
     url = matches[1].chomp('/')
-    sources_list[url] = Mash.new({
-                                    :suite      => matches[2],
-                                    :components => matches[3].split
+    suite = matches[2]
+    components = matches[3].split
+    index = url + '_' +suite
+    sources_list[index] = Mash.new({
+                                  :components => components,
+                                  :suite => suite,
+                                  :server => url
                                   })
   end
 end
