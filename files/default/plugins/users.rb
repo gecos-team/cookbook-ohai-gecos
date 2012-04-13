1b9e37faa7c35c1bc995f4a544b81b74bcdb70cf
David Amian <damian@emergya.com>
2012-04-13 13:11:16 +0200

    Fix user plugins

--- files/default/plugins/users.rb
+++ files/default/plugins/users.rb
@@ -7,13 +7,17 @@ users Array.new
 # LikeWise create the user homes at /home/local/DOMAIN/
 homedirs = Dir["/home/*"] + Dir["/home/local/*/*"]
 homedirs.each do |homedir|
-  Etc.passwd do |entry|
-    next unless homedir == entry.dir
+  temp=homedir.split('/')
+  user=temp[temp.size()-1]
+  begin
+    entry=Etc.getpwnam(user)
     users << Mash.new(
       :username => entry.name,
       :home     => entry.dir,
       :gid      => entry.gid,
       :uid      => entry.uid
     )
+  rescue Exception => e
+    puts 'User ' + user + ' doesn\'t exists'
   end
 end
